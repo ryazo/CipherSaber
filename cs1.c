1ca1d59ab2b8ccc904a0ca11b49ed4df9246d817
Pedro Graca <hexkid@gmail.com>
2010-10-05 01:53:20 +0100

    good night

--- cs1.c
+++ cs1.c
@@ -1,3 +1,9 @@
 #include "cs1.h"
 
-/* ... contents ... */
+/* reading and taking notes
+** user key is a string
+** initialization vector (userkey < 246 bytes (recommended <= 53))
+** initialization vector is not secret
+** rand() should be good enough provided it's been properly seeded
+**
+** I'm tired */
