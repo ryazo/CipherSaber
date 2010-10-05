9b515a4dd437d82cc08419a26668766e4f51a36d
Pedro Graca <hexkid@gmail.com>
2010-10-05 18:28:14 +0100

    ready for testing

--- cs1-test.c
+++ cs1-test.c
@@ -1,6 +1,23 @@
+#include <stdio.h>
 #include "cs1.h"
 
-int main(void) {
-  /* ... contents ... */
+int
+main (void)
+{
+  FILE *h;
+
+  h = fopen ("cstest1.cs1", "rb");
+  if (h)
+    {
+      char data[10000];
+      size_t n = fread (data, 1, 10000, h);
+      if (n)
+	{
+	  unsigned char dst[1000];
+	  setup ();
+	  decrypt (dst, data, n, "asdfg", 5);
+	}
+      fclose (h);
+    }
   return 0;
 }
